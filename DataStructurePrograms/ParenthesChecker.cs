50158cd1609f9bbddd99dca8edbf55979bc1f1e8
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-16 17:20:45 +0530

    [SaiVijayaKumar] Balanced Parentheses code

---
+++ DataStructurePrograms/ParenthesChecker.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructurePrograms
+{
+    class ParenthesChecker
+    {
+        char[] list;
+        int top = 0;
+        public static void ReadInput()
+        {
+            Console.Write("Enter the Arithmetic Expression you want to valid : ");
+            string input = Console.ReadLine();
+            char[] expression = input.ToCharArray();
+            ParenthesChecker parenthes = new ParenthesChecker();
+            bool result = parenthes.Isbalanced(expression);
+            if (result)
+            {
+                Console.WriteLine("Arithmetic Expression has Balanced Parentheses");
+            }
+            else
+            {
+                Console.WriteLine("Arithmetic Expression is  NOT Balanced Parentheses");
+            }
+        }
+        //Push method 
+        public bool Push(char x)
+        {
+            if (top == list.Length)
+            {
+                return false;
+            }
+            else
+            {
+                list[++top] = x;
+                return true;
+            }
+        }
+        //Pop method
+        public char Pop()
+        {
+            if (top == 0)
+            {
+                Console.WriteLine("Underflow error");
+                return '\0';
+            }
+            else
+            {
+                char element = list[top];
+                top--;
+                return element;
+            }
+        }
+        //To check the matching characters to pop from stack
+        public bool MatchingForParentheses(char char1, char char2)
+        {
+            if (char1 == '[' && char2 == ']')
+                return true;
+            else if (char1 == '{' && char2 == '}')
+                return true;
+            else if (char1 == '(' && char2 == ')')
+                return true;
+            else
+                return false;
+        }
+        //Balance method to perform push and pop operation for opening and closing parantheses
+        public bool Isbalanced(char[] exp)
+        {
+            list = new char[exp.Length];
+            bool flag = true;
+            if (exp.Length == 0)
+                return true;
+
+            for (int i = 0; i < exp.Length; i++)
+            {
+
+                if (exp[i] == '{' || exp[i] == '(' || exp[i] == '[')
+                {
+                    Push(exp[i]);
+                    flag = false;
+                }
+                if (exp[i] == '}' || exp[i] == ']' || exp[i] == ')')
+                {
+                    if (MatchingForParentheses(Pop(), exp[i]))
+                        flag = true;
+                    else
+                        return false;
+                }
+            }
+            return flag;
+        }
+    }
+}
--- DataStructurePrograms/Program.cs
+++ DataStructurePrograms/Program.cs
@@ -7,7 +7,7 @@ namespace DataStructurePrograms
         static void Main(string[] args)
         {
             Console.WriteLine("---------------->Data Structure Programs<------------------");
-            Console.WriteLine("1.Unordered List\n2.Order List");
+            Console.WriteLine("1.Unordered List\n2.Order List\n3.Balanced Parentheses");
             Console.WriteLine("-----------------------------------");
             Console.Write("Enter your choice :");
             int choice = Convert.ToInt32(Console.ReadLine());
@@ -18,6 +18,20 @@ namespace DataStructurePrograms
                     break;
                 case 2:
                     OrderedList.Ordered();
+                    break;
+                case 3:
+                    ParenthesChecker.ReadInput();
+                    /*char[] expression = "[(5+6)∗7+8)/(4+3)(5+6)∗(7+8)/(4+3)]".ToCharArray();
+                    ParenthesChecker parenthes = new ParenthesChecker();
+                    bool result = parenthes.Isbalanced(expression);
+                    if (result)
+                    {
+                        Console.WriteLine("Parantheses Balanced");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Parantheses not Balanced");
+                    }*/
                     break;
                 default:
                     Console.WriteLine("Enter valid input");
