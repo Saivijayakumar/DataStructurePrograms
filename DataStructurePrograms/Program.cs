038a4790aeca36ffe9b7a41a5eddd69463e654a4
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-19 14:03:59 +0530

    [SaiVijayaKumar] Displaying Prime Numbers in 2d array

--- DataStructurePrograms/Program.cs
+++ DataStructurePrograms/Program.cs
@@ -7,7 +7,7 @@ namespace DataStructurePrograms
         static void Main(string[] args)
         {
             Console.WriteLine("---------------->Data Structure Programs<------------------");
-            Console.WriteLine("1.Unordered List\n2.Order List\n3.Balanced Parentheses\n4.Finding count of BST");
+            Console.WriteLine("1.Unordered List\n2.Order List\n3.Balanced Parentheses\n4.Finding count of BST\n5.Printing Calender\n6.Prime in Range 0 to 1000");
             Console.WriteLine("-----------------------------------");
             Console.Write("Enter your choice :");
             int choice = Convert.ToInt32(Console.ReadLine());
@@ -28,12 +28,14 @@ namespace DataStructurePrograms
                 case 5:
                     CalenderPrint.PrintCalender();
                     break;
+                case 6:
+                    PrimeRange.MainPrimeMethod();
+                    break;
                 default:
                     Console.WriteLine("Enter valid input");
                     break;
 
             }
-            PrimeRange.MainPrimeMethod();
             Console.Read();
         }
     }
